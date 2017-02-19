ca4a66196cdf3f152b65f40b43e0b30f8afa31a8
malin <malin@yamato.xyz>
2017-02-20 00:30:33 +0100

    first

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
-#/bin/bash
-
+/#!/bin/bash
 #
 # (C) 2017 Yamato Digital Audio
 # Author: Malin af Lääkkö
@@ -10,16 +9,16 @@ src=$GOPATH/packages/src/github.com
 bin=/usr/local/bin
 
 declare -a packages=("MalinYamato/chat")
-declare -a dirs=("css", "images", "js")
+declare -a dirs=("css"  "images"  "js")
 
 
-if [ ! -d ""$document_root" ]; then
+if [ ! -d "$document_root" ]; then
         echo "creating " $document_root/$
         mkdir $document_root
 fi
 
 
-for d in  ${dirs[@]  do
+for d in  "${dirs[@]}" do
     if [ ! -d "$document_root/$d" ]; then
                echo "creating " $document_root/$d
                mkdir $document_root/$d
@@ -28,7 +27,7 @@ done
 
 echo "getting, building and installing packages"
 
-for i in ${packages[@]} do
+for i in ""${packages[@]}"" do
     if [ -d "$src/$i" ]; then
            echo "deleting package " $i
            rm -fr $src/$i
