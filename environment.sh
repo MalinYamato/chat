9ed555df01630e55330b428ea87404de7813c582
malin <malin@yamato.xyz>
2017-03-01 00:39:24 +0100

    first

--- environment.sh
+++ environment.sh
@@ -1,4 +1,5 @@
 
+
 	export GOOGLE_CLIENT_ID="585900153728-tu7nr57i15m1d8sq8ljiv1e00nol2djr.apps.googleusercontent.com"
 	export GOOGLE_CLIENT_SECRET="Qll7wns7E-5uePpE7nqsm56o"
 	export CHAT_HOST="secure.krypin.xyz"
