4ef2a4214ab0851141e65958053b5cdb102b721e
malin <malin@yamato.xyz>
2017-03-26 13:18:17 +0200

    first

--- install.sh
+++ install.sh
@@ -45,7 +45,7 @@ install -v -m +x $GOPATH/bin/* /usr/local/bin
 
 for i in "${packages[@]}"
 do
-   install -v -m +r +x $src/$i/etc/*.sh /etc/supervisor/conf.d
+   install -v -m +r +x $src/$i/etc/*.conf /etc/supervisor/conf.d
    install -v -m +r $src/$i/*.html $document_root
    install -v -m +r $src/$i/js/* $document_root/js
    install -v -m +r $src/$i/css/* $document_root/css
