bd0cbd11662abb1cd0f167807e41174326622b5e
malin <malin@yamato.xyz>
2017-12-24 14:11:25 +0100

    changed env

--- environment.sh
+++ environment.sh
@@ -6,6 +6,6 @@
     export FACEBOOK_CLIENT_SECRET="921f375deba629c6be525f2796b9d4ec"
 
 	export CHAT_HOST="localhost"
-	export CHAT_PRIVATE_KEY="secure.krypin.xyz sfsdf7s89f"
+	export CHAT_PRIVATE_KEY="secure.rakuen.asia sfsdf7s89f"
 
 
