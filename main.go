27fa476b53c94cb7ae90201b24de42469cbb9872
malin <malin@yamato.xyz>
2017-12-24 22:20:38 +0100

    changed env

--- main.go
+++ main.go
@@ -379,7 +379,7 @@ var _persons Persons
 var hub *Hub
 var DocumentRoot string
 var endpoint Endpoint
-var homeTemplate = template.Must(template.ParseFiles("/var/www/krypin/home.html"))
+var homeTemplate = template.Must(template.ParseFiles("/var/www/rakuen/home.html"))
 var sessionStore *sessions.CookieStore
 var _publishers PublishersTargets
 
