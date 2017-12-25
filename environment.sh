f1b4f1bf9734962964cd25f4bf1a39e6aec9fcb1
malin <malin@yamato.xyz>
2017-12-25 05:38:59 +0100

    changed env

--- environment.sh
+++ environment.sh
@@ -3,8 +3,8 @@
 	export GOOGLE_CLIENT_ID="641797937211-t77h5evdsbjl2dbsaeldgiejt97od05l.apps.googleusercontent.com"
 	export GOOGLE_CLIENT_SECRET="qN3LcFeOderLO5UKJrodCpGW"
 
-	export FACEBOOK_CLIENT_ID="1868031936808345"
-    export FACEBOOK_CLIENT_SECRET="921f375deba629c6be525f2796b9d4ec"
+	export FACEBOOK_CLIENT_ID="122591974925861"
+    export FACEBOOK_CLIENT_SECRET="081ab05f1bdcbc166fe0054a467fa18c"
 
 	export CHAT_HOST="localhost"
 	export CHAT_PRIVATE_KEY="secure.rakuen.asia sfsdf7s89f"
