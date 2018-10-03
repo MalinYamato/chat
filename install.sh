9cd6d23bfda6b90148214f9332d16f38886fdac1
MalinYamato <malin@yamato.xyz>
2018-10-03 21:18:31 +0200

    added .PROTOCOL template item

--- install.sh
+++ install.sh
@@ -42,8 +42,6 @@ do
    /usr/local/go/bin/go get github.com/$i
 done
 
-
-
 for i in "${packages[@]}"
 do
    install -v -m +r $src/$i/etc/*.conf /etc/supervisor/conf.d
@@ -52,12 +50,10 @@ do
    install -v -m +r $src/$i/css/* $document_root/css
    install -v -m +r $src/$i/images/* $document_root/images
    install -v -m +r $src/$i/*.html $document_root
-
 done
 
 install -v -m +x $GOPATH/bin/* $document_root
 
-install -v -m +x /etc/letsencrypt/live/$SITE/fullchain.pem $document_root
-install -v -m +x /etc/letsencrypt/live/$SITE/privkey.pem $document_root
+
 
 
