52426f582de248406d71e4da4447bb0606a6c82b
MalinYamato <malin@yamato.xyz>
2018-10-03 15:41:03 +0200

    package management

--- test.go
+++ test.go
@@ -8,9 +8,9 @@ func TestInit() {
 	os.Setenv("CERT", "NO")
 	os.Setenv("PKEY", "NO")
 	os.Setenv("GOOGLE_CLIENT_ID", "641797937211-t77h5evdsbjl2dbsaeldgiejt97od05l.apps.googleusercontent.com")
-	os.Setenv("OOGLE_CLIENT_SECRET", "qN3LcFeOderLO5UKJrodCpGW")
+	os.Setenv("GOOGLE_CLIENT_SECRET", "qN3LcFeOderLO5UKJrodCpGW")
 	os.Setenv("FACEBOOK_CLIENT_ID", "122591974925861")
 	os.Setenv("FACEBOOK_CLIENT_SECRET", "081ab05f1bdcbc166fe0054a467fa18c")
 	os.Setenv("CHAT_HOST", "localhost")
-	os.Setenv("PRIVATE_KEY", "secure.raku.cloud sfsdf7s89f")
+	os.Setenv("CHAT_PRIVATE_KEY", "secure.raku.cloud sfsdf7s89f")
 }
