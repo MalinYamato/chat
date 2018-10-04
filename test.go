43705c867f9b9d50f25ad13f83645e7c96a5a652
MalinYamato <malin@yamato.xyz>
2018-10-04 21:27:59 +0200

    added .PROTOCOL template item

--- test.go
+++ test.go
@@ -11,9 +11,11 @@ func TestInit() {
 	os.Setenv("GOOGLE_CLIENT_SECRET", "qN3LcFeOderLO5UKJrodCpGW")
 	os.Setenv("FACEBOOK_CLIENT_ID", "122591974925861")
 	os.Setenv("FACEBOOK_CLIENT_SECRET", "081ab05f1bdcbc166fe0054a467fa18c")
+	os.Setenv("CHAT_PRIVATE_KEY", "secure.raku.cloud sfsdf7s89f")
 	os.Setenv("PROTOCOL", "http")
 	os.Setenv("HOST", "localhost")
-	os.Setenv("PORT", "8080")
-	os.Setenv("CHAT_PRIVATE_KEY", "secure.raku.cloud sfsdf7s89f")
-
+	os.Setenv("PORT", "9090")
+	os.Setenv("VIDEO_PROTOCOL", "https")
+	os.Setenv("VIDEO_HOST", "media.raku.cloud")
+	os.Setenv("VIDEO_PORT", "8089")
 }
