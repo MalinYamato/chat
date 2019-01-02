0d77de729a352e6743fd0fc658bafc2d40111a42
Malin Yamato Lääkkö <malin@yamato.xyz>
2019-01-02 21:58:03 +0100

    Update main.js
    add utilities. Common Reusable components

--- js/main.js
+++ js/main.js
@@ -28,3 +28,142 @@
 // THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+// Utilities. 
+
+ function ChatURL() {
+            return "{{.Protocol}}://{{.Host}}:{{.Port}}";
+        }
+
+        var _id = 0;
+
+        function getID() {
+            if (_id > 1000) {
+                _id = 0;
+            }
+            _id++;
+            return _id;
+        }
+
+        function getUniqueID(id) {
+            return id + "@" + getID();
+        }
+
+        function getAsync(op, url, cb) {
+            var request = new XMLHttpRequest();
+            request.open("POST", ChatURL() + url, true);
+            request.setRequestHeader('Content-Type', 'application/json');
+            request.responseType = 'json';
+            request.onload = function () {
+                status = request.status;
+                if (status == 200) {
+                    //var json_res =  JSON.parse(request.responseText);
+                    cb && cb(request.response);
+                } else {
+                    console.log("getAsync problem " + op + " " + status)
+                }
+            };
+            request.send(JSON.stringify(op));
+        }
+
+        function getUser(op) {
+            var request = new XMLHttpRequest();
+            request.open("POST", ChatURL() + "/profile", false);
+            request.setRequestHeader('Content-Type', 'application/json');
+            request.send(JSON.stringify(op));
+            if (request.status == 200) {
+                json_response = JSON.parse(request.response);
+                return json_response;
+            } else {
+                console.log("getUser problem " + status)
+            }
+        }
+
+        function logoutAction(op) {
+            var request = new XMLHttpRequest();
+            request.open("POST", ChatURL() + "/logout", false);
+            request.onload = function () {
+                status = request.status;
+                if (status == 200) {
+                    window.location = ChatURL() + "/";
+                } else {
+                    console.log("logoutAction problem " + status)
+                }
+            };
+            request.send({token: "{{.Person.Token }}"});
+        }
+
+        function changeRoom(op, cb) {
+            var request = new XMLHttpRequest();
+            request.open("POST", ChatURL() + "/RoomManager", true);
+            request.setRequestHeader('Content-Type', 'application/json');
+            request.responseType = 'json';
+            request.onload = function () {
+                status = request.status;
+                if (status == 200) {
+                    //var json_res =  JSON.parse(request.responseText);
+                    var info = request.response;
+                    if (info.status.status != "SUCCESS") {
+                        console.log("Calling RooomManger::ChangeRoom returned " + info.status.status + " " + info.status.detail);
+                    }
+                    cb && cb(request.response);
+                } else {
+                    console.log("changeRoom problem " + status)
+                }
+            };
+            request.send(JSON.stringify(op));
+        }
+
+        function AddTarget(op, cb) {
+            var request = new XMLHttpRequest();
+            request.open("POST", ChatURL() + "/TargetManager", true);
+            request.setRequestHeader('Content-Type', 'application/json');
+            request.responseType = 'json';
+            request.onload = function () {
+                status = request.status;
+                if (status == 200) {
+                    //var json_res =  JSON.parse(request.responseText);
+                    var info = request.response;
+                    if (info.status.status != "SUCCESS") {
+                        console.log("Calling TargetManager::Add returned " + info.status.status + " " + info.status.detail);
+                    }
+                    cb && cb(request.response);
+                } else {
+                    console.log("Target::Add problem " + status)
+                }
+            };
+            request.send(JSON.stringify(op));
+        }
+
+        function RemoveTarget(targetId, cb) {
+            var op = {"op": "RemoveTarget", "ids": [targetId]};
+            var request = new XMLHttpRequest();
+            request.open("POST", ChatURL() + "/TargetManager", true);
+            request.setRequestHeader('Content-Type', 'application/json');
+            request.responseType = 'json';
+            request.onload = function () {
+                status = request.status;
+                if (status == 200) {
+                    var info = request.response;
+                    if (info.status.status != "SUCCESS") {
+                        console.log("Calling TargetManager::RemoveTarget returned " + info.status.status + " " + info.status.detail);
+                    } else {
+                        console.log("Delete target requested /targetManager: " + info.status.detail);
+                    }
+                    cb && cb(request.response);
+                } else {
+                    console.log("Target::RemoveTarget problem " + status)
+                }
+            };
+            request.send(JSON.stringify(op))
+        }
+
+        function genderShort(gender) {
+            if (gender == "Female") {
+                return "F"
+            } else if (gender == "Male") {
+                return "M";
+            } else {
+                return "T";
+            }
+        }
